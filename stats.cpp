ae60e3119d12fcf2a14834efde69cc26961d1989
Akshay Chaturvedi <akshayc@adobe.com>
2025-04-25 10:20:33 +0530

    Assignment #1

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,25 @@
 #include "stats.h"
+#include <limits>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<double>& statsVec) {
     //Implement statistics here
+    if (statsVec.empty()) {
+ 		   double nan = std::numeric_limits<double>::quiet_NaN();
+ 		   return Statistics::Stats(nan, nan, nan);
+ 	}
+ 	
+ 	double sum = 0.0;
+ 	double minVal = statsVec[0];
+ 	double maxVal = statsVec[0];
+ 
+ 	for (int i=0; i<statsVec.size(); i++) {
+ 		sum += statsVec[i];
+ 		if (statsVec[i] < minVal) 
+            minVal = statsVec[i];
+ 		if (statsVec[i] > maxVal) 
+            maxVal = statsVec[i];
+ 	}
+ 
+ 	double avg = sum / statsVec.size();
+ 	return Statistics::Stats(avg, minVal, maxVal);
 }
--- stats.h
+++ stats.h
@@ -2,6 +2,14 @@
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
+    struct Stats
+    {
+        double average;
+        double minimum;
+        double maximum;
+        Stats(double avg = 0.0, double min = 0.0, double max = 0.0)
+            : average(avg), minimum(min), maximum(maxVal) {}
+    };
     
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<double>& );
 }
